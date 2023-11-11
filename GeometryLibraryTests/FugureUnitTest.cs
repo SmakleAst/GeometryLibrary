6bb79b88578149c6623b1d74bb1cfcf309de0843
SmakleAst <SmakleLover@yandex.ru>
2023-11-11 23:31:41 +0300

    Implementation of a unit test for the GetArea method of the Circle class.

---
+++ GeometryLibraryTests/FugureUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GeometryLibrary;
+using GeometryLibrary.Figures;
+
+namespace GeometryLibraryTests
+{
+    [TestClass]
+    public class FigureTests
+    {
+        [TestMethod]
+        public void Circle_GetArea_ReturnsCorrectArea()
+        {
+            for (double radius = 0; radius < 100; radius += 0.3)
+            {
+                var circle = new Circle(radius);
+                Assert.AreEqual(Math.Round(Math.PI * radius * radius, 8),
+                    Math.Round(circle.GetArea(), 8));
+            }
+        }
+    }
+}
