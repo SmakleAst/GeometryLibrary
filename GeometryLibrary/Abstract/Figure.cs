5b1c611148a8859c0208c21c223209336afcee05
SmakleAst <SmakleLover@yandex.ru>
2023-11-11 22:20:11 +0300

    Implementation of the abstract class figure

--- GeometryLibrary/Abstract/Figure.cs
+++ GeometryLibrary/Abstract/Figure.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GeometryLibrary.Abstract
+﻿namespace GeometryLibrary.Abstract
 {
-    internal class Figure
+    internal abstract class Figure
     {
+        internal abstract double GetArea();
     }
 }
